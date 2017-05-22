e181a04a8c2c1416c4942fb855a4db07523e5c9e
Nick <nickcica@gmail.com>
2017-05-22 14:38:15 -0400

    Add question 8

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,6 +23,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
+        self.t = 0
 
 
     def reset(self, destination=None, testing=False):
@@ -39,12 +40,18 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
+        self.t += 1.0
+
         if testing:
             self.epsilon = 0.0
-
             self.alpha = 0.0
         else:
-            self.epsilon -= 0.05
+            #self.epsilon -= 0.05
+            #self.epsilon = 1.0/(self.t**2)
+            #self.epsilon = 1.0/(self.t**2 + self.alpha * self.t)
+            #self.epsilon = 1.0/(self.t**2 - self.alpha * self.t)
+            self.epsilon = math.fabs(math.cos(self.alpha*self.t))
+
 
         return None
 
@@ -62,8 +69,21 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set 'state' as a tuple of relevant data for the agent        
-        state = (waypoint,inputs)
-        state = (state[0],tuple(state[1].values()))
+        """state = (waypoint,inputs)
+              state = (state[0],tuple(state[1].values()))
+              return state"""
+
+
+        # helper to create state string
+        def xstr(s):
+            if s is None:
+                return 'None'
+            else:
+                return str(s)
+        
+        state = xstr(waypoint) + "_" + inputs['light'] + "_" + xstr(inputs['left']) + "_" +  xstr(inputs['oncoming'])
+        if self.learning:
+            self.Q[state] = self.Q.get(state, {None:0.0, 'forward':0.0, 'left':0.0, 'right':0.0})
         return state
 
 
@@ -170,7 +190,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True)
+    agent = env.create_agent(LearningAgent, learning=True, epsilon=1.0, alpha=0.01)
     
     ##############
     # Follow the driving agent
@@ -185,14 +205,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.1, log_metrics=True)
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, optimized=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=10, tolerance=0.001)
 
 
 if __name__ == '__main__':
